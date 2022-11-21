ab88bd23f1f5f20d4475770c3e9d84f009f169cd
mayilen <60578759+mayilen@users.noreply.github.com>
2022-11-21 16:32:45 -0500

    CODE

---
+++ src/main/java/com/example/assignment3/Game.java
@@ -0,0 +1,12 @@
+package com.example.assignment3;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Game {
+    public Game(){
+    }
+    public Set<String> suit=new HashSet<>(Arrays.asList("S","C","H","D"));
+}
