aee5b47e67005a7426ea6a19b24aef1af9fb0b7a
mayilen <60578759+mayilen@users.noreply.github.com>
2022-11-21 16:55:58 -0500

    CODE

--- src/main/java/com/example/assignment3/Game.java
+++ src/main/java/com/example/assignment3/Game.java
@@ -8,6 +8,15 @@ public class Game {
     public ArrayList<String> suit=new ArrayList<>(Arrays.asList("S","C","H","D"));
     public ArrayList<String> rank=new ArrayList<>(Arrays.asList("1","2","3","4","5","6","7","8","9","10","J","Q","K"));
     public ArrayList<String> deck = new ArrayList<>();
-
+    public ArrayList<String> deckInitializer(){
+        deck.clear();
+        for(int i=0;i<rank.size();i++){
+            for(int s=0;s<suit.size();s++) {
+                deck.add(rank.get(i)+suit.get(s));
+            }
+        }
+        Collections.shuffle(deck);
+        return deck;
+    }
 
 }
