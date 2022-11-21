675928e02c40b31a886a4956f5b2fee8356a6e44
mayilen <60578759+mayilen@users.noreply.github.com>
2022-11-21 17:05:44 -0500

    REFACTOR

--- src/main/java/com/example/assignment3/Game.java
+++ src/main/java/com/example/assignment3/Game.java
@@ -4,6 +4,7 @@ import java.util.*;
 
 public class Game {
     public Game(){
+        deckInitializer();
     }
     public ArrayList<String> suit=new ArrayList<>(Arrays.asList("S","C","H","D"));
     public ArrayList<String> rank=new ArrayList<>(Arrays.asList("1","2","3","4","5","6","7","8","9","10","J","Q","K"));
