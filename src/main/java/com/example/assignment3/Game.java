029bcae44efac3ad50ed9acc22fc5102f6d85f89
mayilen <60578759+mayilen@users.noreply.github.com>
2022-11-21 16:55:44 -0500

    REFACTOR

--- src/main/java/com/example/assignment3/Game.java
+++ src/main/java/com/example/assignment3/Game.java
@@ -1,15 +1,12 @@
 package com.example.assignment3;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 public class Game {
     public Game(){
     }
-    public Set<String> suit=new HashSet<>(Arrays.asList("S","C","H","D"));
-    public Set<String> rank=new HashSet<>(Arrays.asList("1","2","3","4","5","6","7","8","9","10","J","Q","K"));
+    public ArrayList<String> suit=new ArrayList<>(Arrays.asList("S","C","H","D"));
+    public ArrayList<String> rank=new ArrayList<>(Arrays.asList("1","2","3","4","5","6","7","8","9","10","J","Q","K"));
     public ArrayList<String> deck = new ArrayList<>();
 
 
--- src/test/java/com/example/assignment3/GameTest.java
+++ src/test/java/com/example/assignment3/GameTest.java
@@ -4,14 +4,15 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class GameTest {
+    Game game;
     @Test
     public void getSuit(){
-        Game game = new Game();
+         game = new Game();
         assertEquals(4,game.suit.size());
     }
     @Test
     public void getRank(){
-        Game game = new Game();
+         game = new Game();
         assertEquals(13,game.rank.size());
     }
 }
