4eb5dfd7f9e49222efd45f208d437d98691fa7aa
mayilen <60578759+mayilen@users.noreply.github.com>
2022-11-28 11:18:53 -0500

    CODE

---
+++ src/main/java/com/example/assignment3/Player.java
@@ -0,0 +1,13 @@
+package com.example.assignment3;
+
+import java.util.ArrayList;
+
+public class Player {
+    public int playerID;
+    public Player(int id){
+        playerID=id;
+    }
+public int getPlayerID(){
+        return playerID;
+}
+}
