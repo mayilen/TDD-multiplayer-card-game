8d24832d90956441204c2f287ff916cb8f5cf40e
mayilen <60578759+mayilen@users.noreply.github.com>
2022-11-28 11:23:24 -0500

    CODE

--- src/main/java/com/example/assignment3/Player.java
+++ src/main/java/com/example/assignment3/Player.java
@@ -4,9 +4,14 @@ import java.util.ArrayList;
 
 public class Player {
     public int playerID;
+    public ArrayList<String> cards=new ArrayList<>();
     public Player(int id){
         playerID=id;
     }
+public String drawIntialHand(Game g){
+        cards=g.dealHand();
+        return cards.toString();
+}
 public int getPlayerID(){
         return playerID;
 }
