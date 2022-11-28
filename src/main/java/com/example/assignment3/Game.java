2bc487a0ea9575626d033ebac3371b5a1e9c7ec7
mayilen <60578759+mayilen@users.noreply.github.com>
2022-11-28 14:42:56 -0500

    CODE

--- src/main/java/com/example/assignment3/Game.java
+++ src/main/java/com/example/assignment3/Game.java
@@ -65,6 +65,10 @@ public class Game {
     public String getCardRank(String c){
         return String.valueOf(c.charAt(0));
     }
+    public String playCard(String c){
+        topCard=c;
+        return topCard;
+    }
     public  ArrayList<String>  dealHand(){
         ArrayList<String> hand=new ArrayList<>();
         for(int i=0;i<5;i++){
