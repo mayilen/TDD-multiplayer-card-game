48b6fbb7e25154dfe2af93382823fc2f44d6d285
mayilen <60578759+mayilen@users.noreply.github.com>
2022-11-28 11:23:17 -0500

    U-TEST

--- src/test/java/com/example/assignment3/PlayerTest.java
+++ src/test/java/com/example/assignment3/PlayerTest.java
@@ -2,6 +2,8 @@ package com.example.assignment3;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class PlayerTest {
@@ -11,5 +13,14 @@ public class PlayerTest {
     public void getPlayerID(){
          player = new Player(1);
         assertEquals(1,player.getPlayerID());
+    }
+    @Test
+    public void dealHand()
+    {
+        player = new Player(1);
+        Game game=new Game();
+         ArrayList<String> cards=new ArrayList<>();
+        String c=player.drawIntialHand(game);
+        assertEquals(c,player.cards.toString());
     }
 }
