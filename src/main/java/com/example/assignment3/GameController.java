2bcb1fd46f61f31a5d5beb056a001900e0b2e58e
Mayilen H <mayilen_.h@hotmail.com>
2022-11-30 15:08:09 -0500

    CODE:NETWORK

---
+++ src/main/java/com/example/assignment3/GameController.java
@@ -0,0 +1,43 @@
+package com.example.assignment3;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.*;
+import org.springframework.stereotype.Controller;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+
+import java.util.ArrayList;
+
+@Controller
+public class GameController {
+    @Autowired
+    private SimpMessagingTemplate simpMessagingTemplate;
+    private int counter=-1;
+    private Game game;
+    public ArrayList<Player> players=new ArrayList<>();
+
+    @MessageMapping("/hello")
+    @SendTo("/topic/greetings")
+    public int greet(HelloMessage m) throws Exception {
+        Thread.sleep(500);
+        game=new Game();
+        players.add(new Player(m.getId()));
+        if(players.size()>1){
+
+            System.out.println("sfgsdfg");
+            sendSpecific(players.get(0).getPlayerID(), players.get(0).drawIntialHand(game).toString());
+            sendSpecific(players.get(1).getPlayerID(), players.get(1).drawIntialHand(game).toString());
+
+            simpMessagingTemplate.convertAndSend("/topic/scores",sendScores());
+        }
+        return players.size();
+    }
+
+    @MessageMapping("/room")
+    public void sendSpecific(@DestinationVariable int id,String msg) throws Exception {
+        System.out.println(id);
+        System.out.println(msg);
+        Greeting out = new Greeting(msg);
+        simpMessagingTemplate.convertAndSend( "/user/queue/specific-user/"+id, out);
+    }
+
+}
