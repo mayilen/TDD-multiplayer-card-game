a66aa7c5154fe42aa9ddd8db28690736ebddaf0e
mayilen <60578759+mayilen@users.noreply.github.com>
2022-12-02 13:46:46 -0500

    REFACTOR

--- src/main/java/com/example/assignment3/Player.java
+++ src/main/java/com/example/assignment3/Player.java
@@ -41,53 +41,10 @@ public class Player {
         return false;
     }
 
-public ArrayList<String> drawIntialHand(Game g){
-        cards=g.dealHand();
-        return cards;
-}
 public void setHand(String[] c,Game g){
     g.deck.addAll(cards);
     cards.clear();
     cards.addAll(Arrays.asList(c));
-}
-public ArrayList<String> getCards(){
-        return cards;
-    }
-public ArrayList<String> drawCard(Game g){
-    ArrayList<String> drew=new ArrayList<>();
-    String card;
-        if(g.topCard.charAt(0)=='2'&&g.getDeckSize()>1){
-            card=g.drawCard();
-            cards.add(card);
-            drew.add(card);
-            card=g.drawCard() ;
-            cards.add(card);
-            drew.add(card);
-        }else{
-            card=g.drawCard();
-            cards.add(card);
-            drew.add(card);
-        }
-        return drew;
-
-}
-public boolean playCards(String c,Game g){
-
-        if(g.canPlay(c)){
-            String[] card= c.split(",");
-
-            for(int i=0;i<card.length;i++){
-                if(cards.contains(card[i])){
-                cards.remove(card[i]);
-                g.playCard(card[i]);}
-                else{
-                    return false;
-                }
-            }
-            g.nextTurn();
-            return true;
-        }
-        return false;
 }
 public int getPlayerID(){
         return playerID;
