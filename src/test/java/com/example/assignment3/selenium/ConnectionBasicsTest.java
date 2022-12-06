aefd25d39c23ba05f757844072aaa2358ae0ac62
mayilen <60578759+mayilen@users.noreply.github.com>
2022-12-06 16:08:31 -0500

    REFACTOR

--- src/test/java/com/example/assignment3/selenium/ConnectionBasicsTest.java
+++ src/test/java/com/example/assignment3/selenium/ConnectionBasicsTest.java
@@ -65,46 +65,4 @@ public class ConnectionBasicsTest extends AbstractSeleniumTest {
         p3.quit();
         p4.quit();
     }
-//
-//    @Test
-//    public void canOpenLobby() {
-//        this.indexPage.connect();
-//        assertThat(this.waitForDisplayed(this.indexPage.numberPlayers).isEnabled(), is(true));
-//        this.indexPage.open.click();
-//        assertThat(this.indexPage.hasText("Opening the lobby with specified settings."), is(true));
-//        this.indexPage.disconnect();
-//    }
-//
-//    @Test
-//    public void canStartGame() {
-//        this.indexPage.connect();
-//        this.waitForDisplayed(this.indexPage.numberPlayers).isEnabled();
-//        this.indexPage.numberPlayers.sendKeys("1");
-//        this.indexPage.open.click();
-//        assertThat(this.indexPage.hasText("The game is now ready to begin. Press start when ready."), is(true));
-//        this.indexPage.start.click();
-//        assertThat(this.indexPage.hasText("The game has started! Please wait for your turn."), is(true));
-//        this.indexPage.disconnect();
-//    }
-//
-//    @Test
-//    public void canSeeCardsAfterStart() throws InterruptedException {
-//        this.indexPage.quickStart();
-//        assertThat(this.indexPage.countNumberOfCardsFor(this.indexPage.playerCards), is(2));
-//        assertThat(this.indexPage.countNumberOfCardsFor(this.indexPage.dealerCards), is(2));
-//        assertThat(this.indexPage.countNumberOfCardsFor(this.indexPage.otherPlayer1Cards), is(2));
-//        assertThat(this.indexPage.countNumberOfCardsFor(this.indexPage.otherPlayer2Cards), is(2));
-//
-//        // Want to see if first card is hidden as it should be
-//        final List<WebElement> dealerCards = this.indexPage.getAllCardsFor(this.indexPage.dealerCards);
-//        final String hiddenCardHTML = dealerCards.get(0).getAttribute("innerHTML");
-//        assertThat(hiddenCardHTML.contains("card back"), is(true));
-//
-//        // However, our cards should always be visible.
-//        final List<WebElement> playerCards = this.indexPage.getAllCardsFor(this.indexPage.playerCards);
-//        final String visibleCardHTML = playerCards.get(0).getAttribute("innerHTML");
-//        assertThat(visibleCardHTML.contains("card back"), is(false));
-//        assertThat(visibleCardHTML.contains("<span class=\"rank\">"), is(true));
-//        this.indexPage.disconnect();
-//    }
 }
