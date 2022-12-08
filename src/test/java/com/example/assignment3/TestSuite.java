42fd7ef921fa6717924ef953980ea42bdfc72b09
mayilen <60578759+mayilen@users.noreply.github.com>
2022-12-08 01:35:35 -0500

    REFACTOR

---
+++ src/test/java/com/example/assignment3/TestSuite.java
@@ -0,0 +1,8 @@
+package com.example.assignment3;
+import org.junit.platform.suite.api.SelectClasses;
+import org.junit.platform.suite.api.Suite;
+@Suite
+@SelectClasses({GameTest.class, PlayerTest.class,Assignment3ApplicationTests.class })
+
+public class TestSuite {
+}
